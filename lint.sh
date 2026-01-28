055511f99216029e70b8cbb4fba1c6595abd7766
Ondřej Hošek <ondra.hosek@gmail.com>
2026-01-28 10:51:47 +0100

    add more linters

--- lint.sh
+++ lint.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
-python3 -m pylint \
+
+set -e
+
+echo ruff
+uv run ruff check "prometheus_enocean_exporter.py"
+
+echo ty
+uv run ty check "prometheus_enocean_exporter.py"
+
+echo pylint
+uv run pylint \
 	--disable=C --disable=R \
 	--disable=W0511 \
 	--enable=C0301 \
 	"prometheus_enocean_exporter.py"
+
+echo mypy
+uv run mypy "prometheus_enocean_exporter.py"
